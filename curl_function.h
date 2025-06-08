c56addbd9d08d77c6fcecd9d34812b1006c13810
Mahesh Hire <99095099+maheshbhire@users.noreply.github.com>
2025-06-09 03:29:53 +0530

    Update curl_function.h

--- curl_function.h
+++ curl_function.h
@@ -7,15 +7,22 @@
 
 using namespace std;
 
+// Struct to hold historical stock data
 struct HistoricalData {
+    float open;   // Added open price
     float close;
     float high;
     float low;
     float volume;
 };
 
+// Fetches the current price for a given ticker symbol
 string get_price(const string& ticker_symbol, const string& api);
+
+// Fetches detailed stock quote information
 Json::Value get_stock_quote(const string& ticker_symbol, const string& api);
+
+// Fetches historical data for a given period
 vector<HistoricalData> get_historical_data(const string& ticker_symbol, const string& api, int period);
 
 #endif // CURL_FUNCTION_H
