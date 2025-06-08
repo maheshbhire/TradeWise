15a8ebabb86b337956cddc73da5615a1bec7263d
Mahesh Hire <99095099+maheshbhire@users.noreply.github.com>
2025-06-09 02:42:08 +0530

    Update compile-and-run.sh

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Specify the name of the output executable for main.cpp
+# Specify the names of the output executables
 MAIN_EXECUTABLE="output/trade"
-# Specify the name of the output executable for web_server.cpp
 SERVER_EXECUTABLE="output/web_server"
+FETCH_EXECUTABLE="output/fetch_data"
 
 # Create output directory if it doesn't exist
 mkdir -p output
 
 # Compile main.cpp
 g++ -o "$MAIN_EXECUTABLE" main.cpp curl_functions.cpp functions.cpp -lcurl -ljsoncpp
-
-# Check if main.cpp compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation of main.cpp successful."
 else
@@ -20,12 +18,22 @@ fi
 
 # Compile web_server.cpp
 g++ -o "$SERVER_EXECUTABLE" web_server.cpp curl_functions.cpp functions.cpp -lcurl -ljsoncpp
-
-# Check if web_server.cpp compilation was successful
 if [ $? -eq 0 ]; then
-    echo "Compilation of web_server.cpp successful. Running the web server..."
-    ./"$SERVER_EXECUTABLE"
+    echo "Compilation of web_server.cpp successful."
 else
     echo "Compilation of web_server.cpp failed."
     exit 1
 fi
+
+# Compile fetch_data.cpp
+g++ -o "$FETCH_EXECUTABLE" fetch_data.cpp curl_functions.cpp -lcurl -ljsoncpp
+if [ $? -eq 0 ]; then
+    echo "Compilation of fetch_data.cpp successful."
+else
+    echo "Compilation of fetch_data.cpp failed."
+    exit 1
+fi
+
+# Run the web server
+echo "Running the web server..."
+./"$SERVER_EXECUTABLE"
